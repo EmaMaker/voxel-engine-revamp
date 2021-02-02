d9e5d98de3f6a8cccbcf14f1633d49a23296ae7e
EmaMaker <emamaker0@gmail.com>
2021-02-02 21:49:46 +0100

    generate chunk in a separate thread

--- chunk.py
+++ chunk.py
@@ -3,38 +3,40 @@ from ursina import *
 import utils
 import threading
 
-class Chunk:
+class Chunk(object):
+
+    generated_a_mesh_this_frame = False
+
     def __init__(self, position=Vec3(0, 0, 0)):
         self.position = position
-        self.size = 16
+        utils.chunk_size = 16
 
-        self.needs_update = True
-        self.computed_mesh = False
+        self.requestUpdate()
         self.mesh = None
-
+        
         # Dictionary to store chunks blocks (Vec3 position, BlockType)
         self.blocks = {}
         self.generateOpenSimplex()
 
-
     def update(self):
         self.generate_mesh()
 
-
     # Generate a mesh using quads
     def generate_mesh(self):
-            if self.computed_mesh is True:
-                self.mesh = Entity(model=Mesh(vertices=self.mesh_data[0], triangles=self.mesh_data[1], normals=self.mesh_data[2], colors=self.mesh_data[3]), scale=1)
-                self.mesh.origin = Vec3.zero
-                self.mesh.position = self.position * self.size
-                self.mesh.collider = MeshCollider(self.mesh, mesh=self.mesh.model, center=self.mesh.origin)
+        if self.computed_mesh and not Chunk.generated_a_mesh_this_frame:
+            self.mesh = Entity(model=Mesh(
+                vertices=self.mesh_data[0], triangles=self.mesh_data[1], normals=self.mesh_data[2], colors=self.mesh_data[3]), scale=1)
+            self.mesh.origin = Vec3.zero
+            self.mesh.position = self.position * utils.chunk_size
+            self.mesh.collider = MeshCollider(
+                self.mesh, mesh=self.mesh.model, center=self.mesh.origin)
 
-                self.computed_mesh = False
-
+            self.computed_mesh = False
+            Chunk.generated_a_mesh_this_frame = True
+            #print("Generating mesh for chunk at " + str(self.position))
 
     def compute_mesh(self):
         # print("Updating chunk " + str(self))
-        self.computed_mesh = False
         if self.needs_update is True:
             verts = ()
             tris = ()
@@ -47,78 +49,97 @@ class Chunk:
 
                 # Left
                 if not Vec3(i.x-1, i.y, i.z) in self.blocks or self.blocks.get(Vec3(i.x-1, i.y, i.z)) == BlockType.AIR:
-                    verts = verts + (i, Vec3(i.x, i.y, i.z+1), Vec3(i.x, i.y+1, i.z+1), Vec3(i.x, i.y+1, i.z))
-                    tris = tris + ((len(verts)-4, len(verts)-1, len(verts)-2), (len(verts)-4, len(verts)-2, len(verts)-3))
-                    norms = norms + ((-1,0,0), (-1,0,0), (-1,0,0), (-1,0,0))
-                    colors = colors + (color.red, color.blue, color.lime, color.black)
+                    verts = verts + (i, Vec3(i.x, i.y, i.z+1),
+                                    Vec3(i.x, i.y+1, i.z+1), Vec3(i.x, i.y+1, i.z))
+                    tris = tris + ((len(verts)-4, len(verts)-1, len(verts)-2),
+                                (len(verts)-4, len(verts)-2, len(verts)-3))
+                    norms = norms + ((-1, 0, 0), (-1, 0, 0),
+                                    (-1, 0, 0), (-1, 0, 0))
+                    colors = colors + (color.red, color.blue,
+                                    color.lime, color.black)
                 # Right
                 if not Vec3(i.x+1, i.y, i.z) in self.blocks or self.blocks.get(Vec3(i.x+1, i.y, i.z)) == BlockType.AIR:
-                    verts = verts + (Vec3(i.x+1, i.y, i.z), Vec3(i.x+1, i.y, i.z+1), Vec3(i.x+1, i.y+1, i.z+1), Vec3(i.x+1, i.y+1, i.z))
-                    tris = tris + ((len(verts)-3, len(verts)-2, len(verts)-4), (len(verts)-2, len(verts)-1, len(verts)-4))
-                    norms = norms + ((1,0,0), (1,0,0), (1,0,0), (1,0,0))
-                    colors = colors + (color.red, color.blue, color.lime, color.black)
+                    verts = verts + (Vec3(i.x+1, i.y, i.z), Vec3(i.x+1, i.y, i.z+1),
+                                    Vec3(i.x+1, i.y+1, i.z+1), Vec3(i.x+1, i.y+1, i.z))
+                    tris = tris + ((len(verts)-3, len(verts)-2, len(verts)-4),
+                                (len(verts)-2, len(verts)-1, len(verts)-4))
+                    norms = norms + ((1, 0, 0), (1, 0, 0),
+                                    (1, 0, 0), (1, 0, 0))
+                    colors = colors + (color.red, color.blue,
+                                    color.lime, color.black)
 
                 # Front
                 if not Vec3(i.x, i.y, i.z+1) in self.blocks or self.blocks.get(Vec3(i.x, i.y, i.z+1)) == BlockType.AIR:
-                    verts = verts + (Vec3(i.x, i.y, i.z+1), Vec3(i.x+1, i.y, i.z+1), Vec3(i.x+1, i.y+1, i.z+1), Vec3(i.x, i.y+1, i.z+1))
-                    tris = tris + ((len(verts)-4, len(verts)-1, len(verts)-2), (len(verts)-4, len(verts)-2, len(verts)-3))
-                    norms = norms + ((1,0,0), (1,0,0), (1,0,0), (1,0,0))
-                    colors = colors + (color.red, color.blue, color.lime, color.black)
+                    verts = verts + (Vec3(i.x, i.y, i.z+1), Vec3(i.x+1, i.y, i.z+1),
+                                    Vec3(i.x+1, i.y+1, i.z+1), Vec3(i.x, i.y+1, i.z+1))
+                    tris = tris + ((len(verts)-4, len(verts)-1, len(verts)-2),
+                                (len(verts)-4, len(verts)-2, len(verts)-3))
+                    norms = norms + ((1, 0, 0), (1, 0, 0),
+                                    (1, 0, 0), (1, 0, 0))
+                    colors = colors + (color.red, color.blue,
+                                    color.lime, color.black)
 
                 # Back
                 if not Vec3(i.x, i.y, i.z-1) in self.blocks or self.blocks.get(Vec3(i.x, i.y, i.z-1)) == BlockType.AIR:
-                    verts = verts + (Vec3(i.x, i.y, i.z), Vec3(i.x+1, i.y, i.z), Vec3(i.x+1, i.y+1, i.z), Vec3(i.x, i.y+1, i.z))
-                    tris = tris + ((len(verts)-3, len(verts)-2, len(verts)-4), (len(verts)-2, len(verts)-1, len(verts)-4))
-                    norms = norms + ((1,0,0), (1,0,0), (1,0,0), (1,0,0))
-                    colors = colors + (color.red, color.blue, color.lime, color.black)
+                    verts = verts + (Vec3(i.x, i.y, i.z), Vec3(i.x+1, i.y, i.z),
+                                    Vec3(i.x+1, i.y+1, i.z), Vec3(i.x, i.y+1, i.z))
+                    tris = tris + ((len(verts)-3, len(verts)-2, len(verts)-4),
+                                (len(verts)-2, len(verts)-1, len(verts)-4))
+                    norms = norms + ((1, 0, 0), (1, 0, 0),
+                                    (1, 0, 0), (1, 0, 0))
+                    colors = colors + (color.red, color.blue,
+                                    color.lime, color.black)
 
                 # Top
                 if not Vec3(i.x, i.y+1, i.z) in self.blocks or self.blocks.get(Vec3(i.x, i.y+1, i.z)) == BlockType.AIR:
-                    verts = verts + (Vec3(i.x, i.y+1, i.z), Vec3(i.x+1, i.y+1, i.z), Vec3(i.x+1, i.y+1, i.z+1), Vec3(i.x, i.y+1, i.z+1))
-                    tris = tris + ((len(verts)-3, len(verts)-2, len(verts)-4), (len(verts)-2, len(verts)-1, len(verts)-4))
-                    norms = norms + ((1,0,0), (1,0,0), (1,0,0), (1,0,0))
-                    colors = colors + (color.red, color.blue, color.lime, color.black)
+                    verts = verts + (Vec3(i.x, i.y+1, i.z), Vec3(i.x+1, i.y+1, i.z),
+                                    Vec3(i.x+1, i.y+1, i.z+1), Vec3(i.x, i.y+1, i.z+1))
+                    tris = tris + ((len(verts)-3, len(verts)-2, len(verts)-4),
+                                (len(verts)-2, len(verts)-1, len(verts)-4))
+                    norms = norms + ((1, 0, 0), (1, 0, 0),
+                                    (1, 0, 0), (1, 0, 0))
+                    colors = colors + (color.red, color.blue,
+                                    color.lime, color.black)
 
                 # Back
                 if not Vec3(i.x, i.y-1, i.z) in self.blocks or self.blocks.get(Vec3(i.x, i.y-1, i.z)) == BlockType.AIR:
-                    verts = verts + (Vec3(i.x, i.y, i.z), Vec3(i.x+1, i.y, i.z), Vec3(i.x+1, i.y, i.z+1), Vec3(i.x, i.y, i.z+1))
-                    tris = tris + ((len(verts)-4, len(verts)-1, len(verts)-2), (len(verts)-4, len(verts)-2, len(verts)-3))
-                    norms = norms + ((1,0,0), (1,0,0), (1,0,0), (1,0,0))
-                    colors = colors + (color.red, color.blue, color.lime, color.black)
+                    verts = verts + (Vec3(i.x, i.y, i.z), Vec3(i.x+1, i.y, i.z),
+                                    Vec3(i.x+1, i.y, i.z+1), Vec3(i.x, i.y, i.z+1))
+                    tris = tris + ((len(verts)-4, len(verts)-1, len(verts)-2),
+                                (len(verts)-4, len(verts)-2, len(verts)-3))
+                    norms = norms + ((1, 0, 0), (1, 0, 0),
+                                    (1, 0, 0), (1, 0, 0))
+                    colors = colors + (color.red, color.blue,
+                                    color.lime, color.black)
 
             self.needs_update = False
             self.computed_mesh = True
 
             self.mesh_data = (verts, tris, norms, colors)
 
-
+            #print("Updated mesh for chunk at " + str(self.position))
+        
     def requestUpdate(self):
-        lock = threading.Lock()
-        with lock:
-            self.needs_update = True
-
+        #print("Requested update for " + str(self))
+        self.needs_update = True
+        self.computed_mesh = False
 
     def destroy(self):
         destroy(self.mesh)
 
-
-    def placeBlockAt(self, x,y,z, type):
+    def placeBlockAt(self, x, y, z, type):
         self.blocks.update({Vec3(x, y, z): type})
 
-
     def generatePlain(self):
-        for i in range(0, self.size):
-            for k in range(0, self.size):
-                self.placeBlockAt(i,0,k, BlockType.GRASS)
-
+        for i in range(0, utils.chunk_size):
+            for k in range(0, utils.chunk_size):
+                self.placeBlockAt(i, 0, k, BlockType.GRASS)
 
     def generateOpenSimplex(self):
         self.generatePlain()
-        for i in range(0, self.size):
-            for k in range(0, self.size):
-                value = utils.map( utils.simplex2d( (self.position.x*self.size+i)*0.025, (self.position.z*self.size+k)*0.025) , -1, 1, 0, self.size)
+        for i in range(0, utils.chunk_size):
+            for k in range(0, utils.chunk_size):
+                value = utils.map(utils.simplex2d((self.position.x*utils.chunk_size+i)*0.025,
+                                                  (self.position.z*utils.chunk_size+k)*0.025), -1, 1, 0, utils.chunk_size)
                 for j in range(0, int(value)):
-                    self.placeBlockAt(i,j,k, BlockType.GRASS)
-
-
-                    
+                    self.placeBlockAt(i, j, k, BlockType.GRASS)
--- main.py
+++ main.py
@@ -5,63 +5,81 @@ from ursina.prefabs.first_person_controller import FirstPersonController
 from ursina.shaders import camera_grayscale_shader
 
 from chunk import Chunk
-from utils import *
-
+import utils
 import threading, queue
 
-q = queue.Queue()
-render_distance = 2
-
+compute_mesh_queue = queue.Queue()
+render_distance = 3
+condition = threading.Condition()
 
 def update():
-    for j in range(int(player.position.z/16) - render_distance, int(player.position.z/16)+render_distance):
-        for i in range(int(player.position.x/16) - render_distance, int(player.position.x/16)+render_distance):
-            if  not (Vec3(i,0,j) in world):
-                world.update({Vec3(i,0,j) : Chunk(position=Vec3(i,0,j))})
-                #print("Generating new chunk in {}, {}, {}".format(i,0,j))   
+    condition.acquire()
+    
+    Chunk.generated_a_mesh_this_frame = False
 
-    for vec in world:
-        world[vec].update()
-        q.put(world[vec])
+    for chunk in world.values():
+        chunk.update()
 
-    # for vec in world:
-    #     if vec3dist(player_to_chunk(player.position, world[vec].size), vec) > render_distance+1:
-    #         print("Deleting chunk at " + str(vec))
-    #         world[vec].destroy()
-    #         todelete.append(vec)
-    #     else:
-    #         world[vec].update()
-    #         if(world[vec].needs_update == True):
-    #             q.put(world[vec])
-
-    # for vec in todelete:
-    #     del world[vec]
-    # todelete.clear()
+    condition.release()
 
 def threaded_update():
-    while(1):
-        chunk = q.get()
-        chunk.compute_mesh()
-        q.task_done()
+    while executing:
+        condition.acquire()
+
+        player_pos_chunk = utils.player_to_chunk(player.position)
+        for j in range(int(player_pos_chunk.z) - render_distance, int(player_pos_chunk.z)+render_distance):
+            for i in range(int(player_pos_chunk.x) - render_distance, int(player_pos_chunk.x)+render_distance):
+                if  not (Vec3(i,0,j) in world):
+                    world.update({Vec3(i,0,j) : Chunk(position=Vec3(i,0,j))})
+                    #print("Generating new chunk in {}, {}, {}".format(i,0,j))   
+
+        for vec in world:
+            if utils.vec2dist(utils.player_to_chunk(player.position).xz, vec.xz) > render_distance + 1:
+                #print("Deleting chunk at " + str(vec))
+                world[vec].destroy()
+                todelete.append(vec)
+            else:
+                world[vec].update()
+
+        for vec in todelete:
+            del world[vec]
+        todelete.clear()
+
+        for chunk in world.values():
+            chunk.compute_mesh()
+        condition.release()
+        time.sleep(0.5)
+
+def quit():
+    executing = False
+
 
 if __name__ == "__main__":
-    initUtils()
+    utils.initUtils()
     app = Ursina()
 
     # Dictionary to store world chunks (Vec3 position, Chunk)
     world = {}
     todelete = []
 
-    player = FirstPersonController(world_position=Vec3(8,35,8))
-    collision_zone = CollisionZone(parent=player, radius=32)
-    #player = EditorCamera()
+    #player = FirstPersonController(world_position=Vec3(8,100,8))
+    #collision_zone = CollisionZone(parent=player, radius=32)
+    player = EditorCamera()
+
+    executing = True
 
     try:
-        threading.Thread(target=threaded_update, daemon=True).start()
+        threading.Thread(target=threaded_update).start()
     except:
         print("Couldn't start update thread")
 
     window.vsync = False
     window.show_ursina_splash = True
+    window.borderless = False
+    window.fullscreen= True
+
+    application.development_mode = False
+
+    Sky()
 
     app.run()
